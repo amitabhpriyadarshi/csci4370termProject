e72079fc4307c57f94f769b5f0f58a065bced355
Derek <chiderek@gmail.com>
2015-04-11 16:01:25 -0400

    created containers for header, footer, main content. added jquery-ui files created stubs for the car rental form. created folder for sql files added createTables file

--- src/WebContent/js/app.js
+++ src/WebContent/js/app.js
@@ -6,4 +6,10 @@ $(function(){
 	$('#myButton').on('click', function(){
 		alert('IT WORKS');
 	});
+	$('button').on('click', function(){
+		alert($(this).text() + " clicked.");
+	})
+	
+	$('#startDate').datepicker();
+	$('#endDate').datepicker();
 })
